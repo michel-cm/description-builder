fb293cb988692381e238dae875d6c75495276c74
michel-cm <michel.correa.webart@gmail.com>
2024-08-07 08:28:01 -0300

    reset css inicial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,20 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
+  css: [    
+    '@/assets/css/reset.css',
+  ],
+
   imports: {
     autoImport: true,
   },
+
   srcDir: 'src/',
+
   dir: {
     pages: 'views',
   },
+
   alias: {
     '~': '/src/',
   },
